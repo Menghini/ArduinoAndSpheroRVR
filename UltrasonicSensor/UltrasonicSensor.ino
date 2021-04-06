96f8db2fbcb29cd8809c0e72711c5658492030db
Travis Menghini <tmenghini@micds.org>
2021-04-06 11:15:54 -0500

    Added Comments

--- UltrasonicSensor/UltrasonicSensor.ino
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -1,60 +1,74 @@
-#include <SpheroRVR.h> //Always needed if using a SpheroRVR
-
-
-static uint32_t ledGroup; //Specifically for the lights
-const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor
-const int echoPin = 6; // Echo Pin of Ultrasonic Sensor
-
-
-void setup() {
-  Serial.begin(9600); // Starting Serial Terminal
-
-  // set up communication with the RVR.
-  rvr.configUART(&Serial); //Do not remove or change this line. Always needed if using a SpheroRVR
-  
-  // setting up the led group for both headlights. 
-  ledGroup = 0; //Do not remove or change this line
-  ledGroup |= (1 << static_cast<uint8_t>(LEDs::rightHeadlightRed)); //Do not remove or change this line
-  ledGroup |= (1 << static_cast<uint8_t>(LEDs::rightHeadlightGreen)); //Do not remove or change this line
-  ledGroup |= (1 << static_cast<uint8_t>(LEDs::rightHeadlightBlue)); //Do not remove or change this line
-  ledGroup |= (1 << static_cast<uint8_t>(LEDs::leftHeadlightRed)); //Do not remove or change this line
-  ledGroup |= (1 << static_cast<uint8_t>(LEDs::leftHeadlightGreen)); //Do not remove or change this line
-  ledGroup |= (1 << static_cast<uint8_t>(LEDs::leftHeadlightBlue)); //Do not remove or change this line
-}
-
-void loop() {
-  long duration, inches, cm;
-  pinMode(pingPin, OUTPUT);
-  digitalWrite(pingPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(pingPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(pingPin, LOW);
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-  inches = microsecondsToInches(duration);
-  if(inches < 2){
-    // set headlights to red
-    uint8_t redArray[] = {0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00};
-    rvr.setAllLeds(ledGroup, redArray, sizeof(redArray) / sizeof(redArray[0]));
-  }else{
-    // set headlights to green
-    uint8_t greenArray[] = {0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00};
-    rvr.setAllLeds(ledGroup, greenArray, sizeof(greenArray) / sizeof(greenArray[0]));
-  }
-  cm = microsecondsToCentimeters(duration);
-  Serial.print(inches);
-  Serial.print("in, ");
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  delay(100);
-}
-
-long microsecondsToInches(long microseconds) {
-   return microseconds / 74 / 2;
-}
-
-long microsecondsToCentimeters(long microseconds) {
-   return microseconds / 29 / 2;
-}
+#include <SpheroRVR.h> //Always needed if using a SpheroRVR 
+ 
+ 
+static uint32_t ledGroup; //Specifically for the lights 
+const int pingPin = 7; // Trigger Pin of Ultrasonic Sensor (on the board) 
+const int echoPin = 6; // Echo Pin of Ultrasonic Sensor (on the board) 
+ 
+ 
+void setup() { 
+  Serial.begin(9600); // Starting Serial Terminal 
+ 
+  // set up communication with the RVR. 
+  rvr.configUART(&Serial); //Do not remove or change this line. Always needed if using a SpheroRVR 
+   
+  // setting up the led group for both headlights.  
+  ledGroup = 0; //Do not remove or change this line 
+  ledGroup |= (1 << static_cast<uint8_t>(LEDs::rightHeadlightRed)); //Do not remove or change this line 
+  ledGroup |= (1 << static_cast<uint8_t>(LEDs::rightHeadlightGreen)); //Do not remove or change this line 
+  ledGroup |= (1 << static_cast<uint8_t>(LEDs::rightHeadlightBlue)); //Do not remove or change this line 
+  ledGroup |= (1 << static_cast<uint8_t>(LEDs::leftHeadlightRed)); //Do not remove or change this line 
+  ledGroup |= (1 << static_cast<uint8_t>(LEDs::leftHeadlightGreen)); //Do not remove or change this line 
+  ledGroup |= (1 << static_cast<uint8_t>(LEDs::leftHeadlightBlue)); //Do not remove or change this line 
+} 
+ 
+void loop() { 
+  //Code largely borrowed from https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380 
+  long duration, inches, cm; //Random variables that are integers. 
+   
+  /*  When the sound waves hit the receiver, it turns the Echo pin high for however long the  
+   *  waves were traveling for. To get that, we can use a handy Arduino function called pulseIn().  
+   *  It takes 2 arguments, the pin you are listening to(In our case, the Echo pin), and a state(HIGH or LOW).  
+   *  What the function does is waits for the pin to go whichever state you put in, starts timing, and then  
+   *  stops timing when it switches to the other state. In our case we would put HIGH since we want to start  
+   *  timing when the Echo pin goes high. We will store the time in the duration variable.  
+   *  (It returns the time in microseconds) 
+   */ 
+  pinMode(pingPin, OUTPUT); //tells the pingPin that is output 
+  digitalWrite(pingPin, LOW); 
+  delayMicroseconds(2); 
+  digitalWrite(pingPin, HIGH); 
+  delayMicroseconds(10); 
+  digitalWrite(pingPin, LOW); 
+  pinMode(echoPin, INPUT); 
+ 
+  /* 
+   * Now that we have the time, we can use the equation speed = distance/time, but we will make it time x  
+   * speed = distance because we have the speed. What speed do we have? The speed of sound, of course! 
+   */ 
+  duration = pulseIn(echoPin, HIGH); 
+ 
+  inches = microsecondsToInches(duration); //Sets inches to how far in inches the closest thing is in front of it is 
+   
+  if(inches < 2){  
+    //If an object is less than 2 inches then set the headlights to red 
+    uint8_t redArray[] = {0xFF, 0x00, 0x00, 0xFF, 0x00, 0x00}; 
+    rvr.setAllLeds(ledGroup, redArray, sizeof(redArray) / sizeof(redArray[0])); 
+  }else{ 
+    //if an object is more than 2 inches then set the headlights to green 
+    uint8_t greenArray[] = {0x00, 0xFF, 0x00, 0x00, 0xFF, 0x00}; 
+    rvr.setAllLeds(ledGroup, greenArray, sizeof(greenArray) / sizeof(greenArray[0])); 
+  } 
+  cm = microsecondsToCentimeters(duration); //In case you want it in centimeters. 
+  delay(100); 
+} 
+ 
+//A function that converts microseconds to inches. 
+long microsecondsToInches(long microseconds) { 
+   return microseconds / 74 / 2; 
+} 
+ 
+//A function that converts microseconds to centimeters. 
+long microsecondsToCentimeters(long microseconds) { 
+   return microseconds / 29 / 2; 
+} 
